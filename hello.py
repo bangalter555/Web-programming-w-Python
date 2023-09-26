2db9e51bab5b89d5bf5558966e4f8d914d2098ab
Esteban <estebamlo1975@gmail.com>
2023-09-26 18:20:39 -0300

    first commit

---
+++ hello.py
@@ -0,0 +1,11 @@
+# Say hello to people function
+name = input("What is your name? ")
+print("hello" + " " + name)
+
+# This is
+# Another way of
+# commenting in several lines
+
+"""
+This is another way of commenting
+"""
