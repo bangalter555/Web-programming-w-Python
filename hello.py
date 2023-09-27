16b96c7c94dab310f453c7dd0ad227586aa90d39
Esteban <estebamlo1975@gmail.com>
2023-09-27 17:14:04 -0300

    Changes in content and functions

--- hello.py
+++ hello.py
@@ -2,6 +2,9 @@
 name = input("What is your name? ")
 print("hello,", name)  # Multipl arguments
 
+# New feature in python for escaping:
+surname = input("Wat is your surname? ")
+print(f"Hello, {surname}!")
 # This is
 # Another way of
 # commenting in several lines
